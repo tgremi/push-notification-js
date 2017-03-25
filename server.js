4c09abed67b46fff59d46f9427724f5c133683ab
Thalles Gremi <thallesgremi@gmail.com>
2017-03-25 13:33:33 -0300

    changes

--- server.js
+++ server.js
@@ -1,33 +1,28 @@
-/*  */
-
 var express = require('express')
-    , app = express()
-    , server = require('http').createServer(app).listen(4555)
-    , io = require('socket.io').listen(server)
-    , bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-var port = process.env.PORT || 8080;
-var router = express.Router();
-/* Chamada do socket */
-app.use(send);
-app.use('/api', router);
-router.route('/notification')
-    .get(function (req, res) {
-        /* Ao acessar o caminho definido na API "http://localhost/api/notification*/
-        res.json({ message: "Testing route" })
+  , app = express()
+  , server = require('http').createServer(app).listen(4555)
+  , io = require('socket.io').listen(server)
+  , bodyParser = require('body-parser');
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  var port = process.env.PORT || 8080;
+  var router = express.Router();
+  /* Socket irá aqui depois */
+  var emitir = function(req, res, next){
+	var notificar = req.query.notificacao || '';
+		if(notificar != '')	 {
+		io.emit('notificacao', notificar);
+		next();
+	} else {
+			next();
+		}
+	}
+  app.use(emitir);
+  app.use('/api', router);
+  router.route('/notificar')
+    .get(function(req, res){
+    //aqui vamos receber a mensagem
+    res.json({message: "testando essa rota"})
     })
-app.listen(port);
-console.log('Port connected ' + port);
-
-/* Emite a notificação recebida através da Rota Api, utilizando um Middleware*/
-var send = function (req, res, next) {
-    var notification = req.query.notificacao || '';
-    /* Se a mensagem for diferente de vazia emito ao cliente*/
-    if (notification != '') {
-        io.emit('notificacao', notification);
-        next();
-    } else {
-        next();
-    }
-}
+  app.listen(port);
+  console.log('conectado a porta ' + port);
